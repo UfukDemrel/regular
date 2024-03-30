c16a436807ca38998c82933889bcf905d65ac594
UfukDemrel <udemirel012@gmail.com>
2024-03-30 19:21:52 +0300

    first commit

--- src/board.js
+++ src/board.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import mordecai from "./images/mordecai.png";
 import rigby from "./images/rigby.png";
 import benson from "./images/benson.png";
@@ -10,6 +10,7 @@ import { useStore } from "./store";
 const Board = () => {
   const [modal, setModal] = useState(false);
   const { tasks, addTask, moveTask } = useStore();
+  const inputRef = useRef(null);
 
   const handleClick = () => {
     setModal(!modal);
@@ -22,6 +23,12 @@ const Board = () => {
       useStore.setState({tasks: parsedTasks});
     }
   },[]);
+
+  useEffect(() => {
+    if (modal && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [modal]);
 
   const handleAddTask = (e) => {
     if (e.key === "Enter" && e.target.value.trim() !== "") {
@@ -59,9 +66,13 @@ const Board = () => {
     console.log(column);
     const updatedTasks = { ...tasks };
     updatedTasks[column][index] = e.target.value;
-    console.log(updatedTasks);
     localStorage.setItem("tasks", JSON.stringify(updatedTasks))
     useStore.setState({ tasks: updatedTasks });
+    console.log(updatedTasks);
+  }
+
+  const handleSvg = () =>{
+    setModal(false);
   }
 
   const renderTasks = (column) =>
@@ -81,10 +92,14 @@ const Board = () => {
     <>
       {modal && (
         <div className="add-task">
-          <div className="add-box">
+          <div className="text-center bg-gradient-to-br from-red-500 via-transparent to-blue-500 p-6 rounded-xl shadow">
+          <svg onClick={handleSvg} className="cursor-pointer flex justify-end text-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="1rem" height="1rem"><path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"/></svg> 
+            <img className="w-60" src="https://upload.wikimedia.org/wikipedia/tr/6/61/Regular_Show.png" alt="alt" />
+            <div className="mb-2 font-semibold text-xl">Give them a mission.</div>
             <input
+              className="shadow bg-transparent"
+              ref={inputRef}
               type="text"
-              placeholder="Create New Tasks.."
               onKeyDown={handleAddTask}
             />
           </div>
@@ -115,9 +130,9 @@ const Board = () => {
         </svg>
       </div>
 
-      <div className="flex gap-4 w-full">
+      <div className="flex gap-4 w-full flex-wrap">
         <div
-          className="w-80 h-auto bg-slate-400 rounded-md p-2 shadow"
+          className="w-72 h-auto bg-slate-400 rounded-md p-2 shadow"
           onDragOver={handleDragOver}
           onDrop={(e) => handleDrop(e, "mordecai")}
         >
@@ -129,7 +144,7 @@ const Board = () => {
         </div>
 
         <div
-          className="w-80 h-auto bg-amber-600 rounded-md p-2 shadow"
+          className="w-72 h-auto bg-amber-600 rounded-md p-2 shadow"
           onDragOver={handleDragOver}
           onDrop={(e) => handleDrop(e, "rigby")}
         >
@@ -141,7 +156,7 @@ const Board = () => {
         </div>
 
         <div
-          className="w-80 h-auto bg-red-500 rounded-md p-2 shadow"
+          className="w-72 h-auto bg-red-500 rounded-md p-2 shadow"
           onDragOver={handleDragOver}
           onDrop={(e) => handleDrop(e, "benson")}
         >
@@ -153,7 +168,7 @@ const Board = () => {
         </div>
 
         <div
-          className="w-80 h-auto bg-rose-300 rounded-md p-2 shadow"
+          className="w-72 h-auto bg-rose-300 rounded-md p-2 shadow"
           onDragOver={handleDragOver}
           onDrop={(e) => handleDrop(e, "pops")}
         >
@@ -165,7 +180,7 @@ const Board = () => {
         </div>
 
         <div
-          className="w-80 h-auto bg-slate-300 rounded-md p-2 shadow"
+          className="w-72 h-auto bg-slate-300 rounded-md p-2 shadow"
           onDragOver={handleDragOver}
           onDrop={(e) => handleDrop(e, "skips")}
         >
@@ -177,7 +192,7 @@ const Board = () => {
         </div>
 
         <div
-          className="w-80 h-auto bg-cyan-300 rounded-md p-2 shadow"
+          className="w-72 h-auto bg-cyan-300 rounded-md p-2 shadow"
           onDragOver={handleDragOver}
           onDrop={(e) => handleDrop(e, "done")}
         >
