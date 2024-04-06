0708ee20a254319b52497d7370219b983df6227e
UfukDemrel <udemirel012@gmail.com>
2024-04-06 22:39:16 +0300

    first commit

--- src/board.js
+++ src/board.js
@@ -95,18 +95,15 @@ const Board = () => {
     <>
       {modal && (
         <div className="add-task">
-          <div className="text-center bg-gray-400 p-6 rounded-xl shadow">
-          <svg onClick={handleSvg} className="cursor-pointer flex justify-end text-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="1rem" height="1rem"><path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"/></svg> 
-            <img className="w-60" src="https://upload.wikimedia.org/wikipedia/tr/6/61/Regular_Show.png" alt="alt" />
+          <div className="text-center bg-gray-200 p-6 rounded-xl shadow">
             <div className="mb-2 font-semibold text-xl">Give them a mission.</div>
-            <div className="flex justify-center mb-3">
-              <input className="shadow-lg bg-transparentborder-black" ref={inputRef} type="text"/>
+            <div className="flex justify-center mb-3 shadow-2xl">
+              <input className="shadow-lg bg-white" ref={inputRef} type="text"/>
               </div>
-              <div className="flex justify-center">
-              <button onClick={handleAddTask}>Buton</button>
+              <div className="flex justify-center gap-3">
+              <button className="shadow-lg bg-transparent text-black border-black border-2 w-2/4 rounded-md font-semibold p-1" onClick={handleAddTask}>Add Task</button>
+              <button onClick={handleSvg} className="hover:shadow-lg text-red-600 bg-transparent border-red-600 border-2 w-2/4 rounded-md font-semibold p-1">Cancel</button>
               </div>
-              
-            
           </div>
         </div>
       )}
