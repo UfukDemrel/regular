7c262384a2479e73ebe6b1077e3dd520ddcee967
UfukDemrel <udemirel012@gmail.com>
2024-04-06 21:56:30 +0300

    first commit

--- src/board.js
+++ src/board.js
@@ -32,10 +32,11 @@ const Board = () => {
     }
   }, [modal]);
 
-  const handleAddTask = (e) => {
-    if (e.key === "Enter" && e.target.value.trim() !== "") {
-      addTask("mordecai", e.target.value.trim());
-      e.target.value = "";
+  const handleAddTask = () => {
+    const inputValue = inputRef.current.value.trim();
+    if (inputValue !== "") {
+      addTask("mordecai", inputValue);
+      inputRef.current.value = "";
       setModal(false);
     }
   };
@@ -98,12 +99,14 @@ const Board = () => {
           <svg onClick={handleSvg} className="cursor-pointer flex justify-end text-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="1rem" height="1rem"><path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"/></svg> 
             <img className="w-60" src="https://upload.wikimedia.org/wikipedia/tr/6/61/Regular_Show.png" alt="alt" />
             <div className="mb-2 font-semibold text-xl">Give them a mission.</div>
-            <input
-              className="shadow bg-transparent"
-              ref={inputRef}
-              type="text"
-              onKeyDown={handleAddTask}
-            />
+            <div className="flex justify-center mb-3">
+              <input className="shadow-lg bg-transparentborder-black" ref={inputRef} type="text"/>
+              </div>
+              <div className="flex justify-center">
+              <button onClick={handleAddTask}>Buton</button>
+              </div>
+              
+            
           </div>
         </div>
       )}
--- src/store.js
+++ src/store.js
@@ -1,32 +1,28 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
 
 export const useStore = create((set) => ({
-    tasks: {
-        mordecai: [],
-        rigby: [],
-        benson: [],
-        pops: [],
-        skips: [],
-        muscleman: [],
-        highfive: [],
-        done: []
-      },
-      addTask: (column, task) =>
-      set((state) => {
-        const updatedTasks = { ...state.tasks, [column]: [...state.tasks[column], task] };
-        console.log("Add Tasks:",updatedTasks);
-        localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-
-        return { tasks: updatedTasks };
-      }),
-    moveTask: (source, destination, taskIndex) =>
-      set((state) => {
-        const updatedTasks = { ...state.tasks };
-        const [movedTask] = updatedTasks[source].splice(taskIndex, 1);
-        updatedTasks[destination].push(movedTask);
-        localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-        console.log("Move Tasks:",updatedTasks);
-        return { tasks: updatedTasks };
-      }),    
+  tasks: {
+    mordecai: [],
+    rigby: [],
+    benson: [],
+    pops: [],
+    skips: [],
+    muscleman: [],
+    highfive: [],
+    done: []
+  },
+  addTask: (column, task) =>
+    set((state) => {
+      const updatedTasks = { ...state.tasks, [column]: [...state.tasks[column], task] };
+      localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+      return { tasks: updatedTasks };
+    }),
+  moveTask: (source, destination, taskIndex) =>
+    set((state) => {
+      const updatedTasks = { ...state.tasks };
+      const [movedTask] = updatedTasks[source].splice(taskIndex, 1);
+      updatedTasks[destination].push(movedTask);
+      localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+      return { tasks: updatedTasks };
+    })
 }));
-
